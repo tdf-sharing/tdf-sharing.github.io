75f65fc1119503812b899b5d5f013f81dac2e39d
Companion <98676156+tdf-sharing@users.noreply.github.com>
2023-12-29 23:30:54 +0800

    Add files via upload

---
+++ E-Shop/assets/throttle-8d3b8ec0.js
@@ -0,0 +1,1 @@
+import{d as n}from"./debounce-c7ad9d3e.js";import{a6 as l}from"./index-6ff172c9.js";var g="Expected a function";function u(t,a,r){var i=!0,e=!0;if(typeof t!="function")throw new TypeError(g);return l(r)&&(i="leading"in r?!!r.leading:i,e="trailing"in r?!!r.trailing:e),n(t,a,{leading:i,maxWait:a,trailing:e})}export{u as t};
